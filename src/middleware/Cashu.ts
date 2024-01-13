9110faf27b03f1f37548b801d23e19a3e6667dfd
kody.low <kodylow7@gmail.com>
2024-01-12 16:40:39 -0800

    docs: cashu docs

--- src/middleware/Cashu.ts
+++ src/middleware/Cashu.ts
@@ -2,12 +2,29 @@ import { getDecodedToken, TokenEntry } from "@cashu/cashu-ts";
 
 import { CLIENTS, CONFIG } from "..";
 
+/**
+ * X-Cashu is a single round push payment.
+ * The client sends an encoded cashu token in the header for the exact amount of the route
+ * "X-Cashu: cashuAeyJ0b2tlbiI6W3sicHJvb2ZzIjpbeyJpZCI6IkkyeU4raVJZZmt6VCIsImFtb3VudCI6MSwiQyI6IjAyZTc2ZTE3ZWM0MTVjNjk5YzAzYjAxYWRlY2Q3ZjMwOThlNmVmMjk3ODBmYWE5ZGZlZmQ1NGVkNWNmNjVmOTE1YiIsInNlY3JldCI6ImpUcWdQNDEwaHZ2WllKMnplZGtWNVhsbnUvSkxBMVRXTlZMWFhEOHc4MHM9In1dLCJtaW50IjoiaHR0cHM6Ly84MzMzLnNwYWNlOjMzMzgifV19"
+ * 
+ * The token's proofs MUST match the configured mintURL.
+ * The token's amount MUST match the exact route cost. (If overpaid or underpaid, just don't redeem and throw an error)
+ * 
+ * The server MUST redeem the token for the exact amount of the route.
+ * 
+ * Extremely useful for one time payments like a single API call.
+ * Server can be completely stateless, all state management is done by the mint.
+ * 
+ * @param encodedToken 
+ * @param exactRouteCost 
+ * @returns boolean
+ */
 export const middleware_cashu = async (
   xCashuHeader: string,
-  exactAmount: number,
+  exactRouteCost: number,
 ): Promise<boolean> => {
   try {
-    const token = tokenIsValid(xCashuHeader, exactAmount);
+    const token = tokenIsValid(xCashuHeader, exactRouteCost);
     console.log("token", token);
 
     if (token) {
